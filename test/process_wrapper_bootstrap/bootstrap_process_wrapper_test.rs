d83c86581cd444fc4b60fd081d551dbfd71af1cb
UebelAndre <github@uebelandre.com>
2024-06-13 09:37:40 -0700

    Add flag to refer to a `sh_toolchain` for process wrapper bootstrap shebangs (#2694)
    This introduces the flag
    `--@rules_rust//rust/settings:experimental_use_sh_toolchain_for_bootstrap_process_wrapper`
    which can be used to embed the shell path from
    `@bazel_tools//tools/sh:toolchain_type` in the rustc bootstrap process
    wrapper.

---
+++ test/process_wrapper_bootstrap/bootstrap_process_wrapper_test.rs
@@ -0,0 +1,23 @@
+//! Tests for the boostrap process wrapper
+
+use std::fs::read_to_string;
+
+use runfiles::Runfiles;
+
+/// Test that the shell process wrapper starts with the expected shebang to
+/// avoid breaking the contract with the `bootstrap_process_wrapper` rule.
+#[test]
+fn test_shebang() {
+    let rfiles = Runfiles::create().unwrap();
+
+    let script = runfiles::rlocation!(
+        rfiles,
+        "rules_rust/util/process_wrapper/private/process_wrapper.sh"
+    );
+
+    let content = read_to_string(script).unwrap();
+    assert!(
+        content.starts_with("#!/usr/bin/env bash"),
+        "The shell script does not start with the expected shebang."
+    )
+}
